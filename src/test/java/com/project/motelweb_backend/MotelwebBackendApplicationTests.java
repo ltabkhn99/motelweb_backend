a8b435cac2da798155305ddf8219a04d78b558aa
ltabkhn99 <letuananh7111999@gmail.com>
2024-01-16 22:41:17 +0700

    init project

---
+++ src/test/java/com/project/motelweb_backend/MotelwebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.motelweb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotelwebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
